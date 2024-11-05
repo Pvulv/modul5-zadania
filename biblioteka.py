69cf2d739fadfbdd49ffb5a0cd2928f47dfb0df9
Paulina Wróbel <paulinawro13579@gmail.com>
2024-11-05 16:42:57 +0100

    define search, get conditions and input form user

--- biblioteka.py
+++ biblioteka.py
@@ -54,3 +54,56 @@ def getSeries():
     for serie in Movies_Series:
         if isinstance(serie,Series) == True:
             print(serie)
+
+def search():
+    search_again = True
+    while search_again:
+        question_1 = input("Chcesz zobaczyć listę wszystkich filmów czy seriali? (Wpisz 'M' dla filmów lub 'S' dla seriali): ").strip().upper()
+        
+        if question_1 == 'M':
+            getMovie()
+            question_2 = input('Wybierz, który film chcesz obejrzeć (Napisz dokładny tytuł): ').strip()
+            found = False
+            for item in Movies_Series:
+                if isinstance(item, Movie) and item.title.lower() == question_2.lower():
+                    item.Play
+                    print(f"Obejrzano film: {item.title}. Liczba wyświetleń: {item.num_views}")
+                    found = True
+                    break
+            if not found:
+                print("Nie znaleziono filmu o podanym tytule.")
+                
+        elif question_1 == 'S':
+            getSeries()
+            question_2 = input('Wybierz, który serial chcesz obejrzeć (Napisz dokładny tytuł): ').strip()
+            found = False
+            for item in Movies_Series:
+                if isinstance(item, Series) and item.title.lower() == question_2.lower():
+                    print(f"\nWybrano serial: {item.title}")
+                    print("Lista dostępnych odcinków:")
+                    print(item.display_episodes())
+                    episode_choice = input("Wybierz numer sezonu i odcinka w formacie S01E01: ").strip().upper()
+                    
+                    episode_found = False
+                    for season_num, num_ep in enumerate(item.num_season, start=1):
+                        for episode_num in range(1, num_ep + 1):
+                            episode_id = f"S{season_num:02}E{episode_num:02}"
+                            if episode_id == episode_choice:
+                                print(f"Obejrzano {episode_id} z serialu {item.title}")
+                                item.Play
+                                episode_found = True
+                                break
+                        if episode_found:
+                            break
+                    if not episode_found:
+                        print("Nie znaleziono takiego odcinka.")
+                    found = True
+                    break
+            if not found:
+                print("Nie znaleziono serialu o podanym tytule.")
+        else:
+            print("Podano złą wartość, spróbuj ponownie!")
+        
+        search_again = input('Czy chcesz kontynuować (T lub N): ').strip().upper()
+        if search_again == 'N':
+            search_again = False
