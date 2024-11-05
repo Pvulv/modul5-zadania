1271ef87455e074818123421d10bb972332382f1
Paulina Wróbel <paulinawro13579@gmail.com>
2024-11-05 16:27:16 +0100

    add class BusinessContact

--- wizytowki.py
+++ wizytowki.py
@@ -25,3 +25,12 @@ class BaseContact(BusinessCards):
         print(f" Wybieram numer {self.priv_number} i dzwonię do {self.name} {self.last_name}")
     def __str__(self):
         return f"{self.name}, {self.last_name}, {self.email}, {self.priv_number}"
+
+class BusinessContact(BusinessCards):
+    def __init__(self, name, last_name, company, job, business_number):
+        super().__init__(name, last_name, company, job, None)
+        self.business_number = business_number
+    def contact(self):
+        print(f" Wybieram numer {self.business_number} i dzwonię do {self.name} {self.last_name}")
+    def __str__(self):
+        return f"{self.company}, {self.job}, {self.business_number}"
