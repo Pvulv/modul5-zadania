7ea67ecdc1e6c70ad23d3fb87c0ad1a0581a153b
Paulina Wróbel <paulinawro13579@gmail.com>
2024-11-05 16:37:41 +0100

    add class Series and define display_episodes

--- biblioteka.py
+++ biblioteka.py
@@ -28,3 +28,17 @@ class BaseInfo:
 class Movie(BaseInfo):
     def __init__(self, title, year, category, num_views = 0):
         super().__init__(title, year, category, num_views)
+
+class Series(BaseInfo):
+    def __init__(self, title, year, category, num_views = 0, num_season = []):
+        super().__init__(title, year, category, num_views)
+        self.num_season = num_season
+    
+    def display_episodes(self):
+        episode_list = [f"S{season_num:02}E{episode_num:02}"
+                        for season_num, num_ep in enumerate(self.num_season, start=1)
+                        for episode_num in range(1, num_ep + 1)]
+        return "\n".join(episode_list)
+    
+    def __str__(self):
+        return f"{self.title}"
